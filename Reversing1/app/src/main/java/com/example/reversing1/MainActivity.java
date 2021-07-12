a44eebca8ca3ad34cec4478e385f35f1c7eabe0d
병준 김 <q1q5q9@naver.com>
2021-07-12 15:59:57 +0900

    Initial commit

---
+++ Reversing1/app/src/main/java/com/example/reversing1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.reversing1;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getResources().getConfiguration().orientation ==
+                Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(R.layout.activity_portrait);
+        } else {
+            setContentView(R.layout.activity_landscape);
+        }
+    }
+}
